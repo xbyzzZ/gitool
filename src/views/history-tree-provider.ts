e4c11de65da9b13f2de7cba2ac7609d9cb7a47d9
许博阳 <xuboyang@irptech.cn>
2026-08-02 17:49:14 +0800

    回滚：恢复提交历史引用显示

--- src/views/history-tree-provider.ts
+++ src/views/history-tree-provider.ts
@@ -56,7 +56,7 @@ function relativeTime(authoredAt: string, now = new Date()): string {
 }
 
 function refLabel(ref: CommitGraphNode['refs'][number]): string {
-  return ref.name;
+  return ref.kind === 'head' ? `HEAD ${ref.name}` : ref.name;
 }
 
 function commitIcon(commit: CommitGraphNode): vscode.ThemeIcon {
